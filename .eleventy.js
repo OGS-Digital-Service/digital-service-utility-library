caf5fa850bec7c0da17a2c93bd95fa0f1f1904eb
Angie <lewisangelique.c@gmail.com>
2026-06-05 10:28:16 -0400

    attempt to merge nys design system

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,14 @@
 module.exports = function(eleventyConfig){
 
+// Copy NYSDS CSS from node_modules into the compile css files
+eleventyConfig.addPassthroughCopy({
+    "./node_modules/@nysds/styles/dist/nysds-full.min.css": "css/nysds-full.min.css"
+});
+
+// Copy NYSDS JS from node_modules into the compile js files
+eleventyConfig.addPassthroughCopy({
+    "./node_modules/@nysds/components/dist/nysds-full.min.js": "js/nysds-full.min.js"
+});
 
     eleventyConfig.setTemplateFormats([
         "pdf",
